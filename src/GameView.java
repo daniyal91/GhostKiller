c7c8f1c6bb32b2ba72f26c4c525bbd9281019674
Daniyal Rao <raothegreat_dani_91@hotmail.com>
2016-01-27 13:50:43 -0500

    Add images for the tower, health and bank. Also added tower selection panel and health and bank panel to the game view window.

--- src/GameView.java
+++ src/GameView.java
@@ -1,9 +1,13 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import model.GameGrid;
 
@@ -17,6 +21,13 @@ public class GameView {
         int col = gameGrid.getCases()[0].length;
 
         this.gameFrame = new JFrame("Tower defense game");
+        
+        // mainPane to add all other panels
+        JPanel mainPane = new JPanel();
+        mainPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        mainPane.setLayout(new BorderLayout(0, 0));
+        this.gameFrame.setContentPane(mainPane);
+        
         JPanel map = new JPanel(new GridLayout(row, col, 0, 0));
 
         for (int i = 0; i < row * col; i++) {
@@ -34,13 +45,44 @@ public class GameView {
 
             map.add(tile);
         }
-
-        this.gameFrame.setContentPane(map);
-        this.gameFrame.setSize(400 * col / 10, 400 * row / 10);
+        
+        mainPane.add(map);
+        
+        //this.gameFrame.setContentPane(map);
+        this.gameFrame.setSize(540 * col / 10, 820 * row / 10);
         this.gameFrame.setLocationRelativeTo(null);
         this.gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.gameFrame.setVisible(true);
-
+        
+        //Area where towers are displayed
+        JPanel towerSelectionArea = new JPanel();
+        towerSelectionArea.setBackground(Color.DARK_GRAY);
+        mainPane.add(towerSelectionArea, BorderLayout.NORTH);
+        JLabel towerSelectionText = new JLabel("Towers");
+        towerSelectionText.setForeground(Color.white);
+        towerSelectionArea.add(towerSelectionText);
+        // Tower images
+        String[] towers= {"icons/AncientTower.png","icons/KingTower.png","icons/ModernTower.png"};
+        for(int i = 0; i < towers.length; i++) {
+        	JLabel imgLabel = new JLabel(new ImageIcon(towers[i]));
+        	towerSelectionArea.add(imgLabel);
+        }
+        // Health and bank panel
+        JPanel healthBankPanel = new JPanel();
+        healthBankPanel.setBackground(Color.DARK_GRAY);
+        mainPane.add(healthBankPanel, BorderLayout.SOUTH);
+        // Bank image
+        JLabel bankImgLabel = new JLabel(new ImageIcon("icons/bank_icon.png"));
+        healthBankPanel.add(bankImgLabel);
+        JLabel bankTxt = new JLabel("$100");
+        bankTxt.setForeground(Color.green);
+        healthBankPanel.add(bankTxt);
+        // Health image
+        JLabel lifeImgLabel = new JLabel(new ImageIcon("icons/life_icon.png"));
+        healthBankPanel.add(lifeImgLabel);
+        JLabel lifeTxt = new JLabel("75%");
+        lifeTxt.setForeground(Color.green);
+        healthBankPanel.add(lifeTxt);
     }
 
 
