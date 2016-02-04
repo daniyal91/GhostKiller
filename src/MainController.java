3393167e91592a5c3e874c91c3b93dae2164e061
Louis-Bertrand Varin <louisbvarin@gmail.com>
2016-02-03 23:32:58 -0500

    Created the views packages

--- src/MainController.java
+++ src/MainController.java
@@ -1,11 +1,19 @@
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
-import javax.swing.JFrame;
+import javax.swing.JFileChooser;
+
+import model.GameGrid;
+import views.EditMapView;
+import views.GameView;
+import views.MainView;
 
 
-public class MainController implements Runnable {
+public class MainController implements Runnable, ActionListener {
 
-    private JFrame mainFrame;
+    private MainView mainFrame;
 
 
     public static void main(String[] args) {
@@ -18,11 +26,62 @@ public class MainController implements Runnable {
     @Override
     public void run() {
         try {
-            this.mainFrame = new MainFrame();
+            this.mainFrame = new MainView(this);
             this.mainFrame.setVisible(true);
         } catch (Exception exception) {
             exception.printStackTrace();
         }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        if (event.getSource() == this.mainFrame.buttonCreate) {
+
+            String lineText = this.mainFrame.textFieldLines.getText();
+            int lineCount = Integer.parseInt(lineText);
+            String columnText = this.mainFrame.textFieldColumns.getText();
+            int columnCount = Integer.parseInt(columnText);
+            int[][] mapArr = new int[lineCount][columnCount];
+            EditMapView mapView = new EditMapView(mapArr);
+
+        } else if (event.getSource() == this.mainFrame.buttonEdit) {
+
+            String filePath = this.selectFile();
+            if (filePath != null){
+
+                GameGrid gameGrid = new GameGrid();
+                gameGrid.readFromFile(filePath);
+                EditMapView mapView = new EditMapView(gameGrid.cases);
+
+            }
+
+        } else if (event.getSource() == this.mainFrame.buttonLoad) {
+            String filePath = this.selectFile();
+            if (filePath != null){
+
+                GameGrid gameGrid = new GameGrid();
+                gameGrid.readFromFile(filePath);
+                GameView mapView = new GameView(gameGrid);
+
+            }
+        }
+    }
+
+    /**
+     * Wrapper around JFileChooser.
+     *
+     * @return The path of the selected file, or null if no file selected.
+     */
+    private String selectFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        File currentDir = new File(System.getProperty("user.dir"));
+        fileChooser.setCurrentDirectory(currentDir);
+        if(fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            return fileChooser.getSelectedFile().getName();
+        } else {
+            return null;
+        }
+
     }
 
 }
