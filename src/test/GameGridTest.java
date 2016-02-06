3553af28b66a99deaffabc7b9181fc00c649b7ee
Louis-Bertrand Varin <louisbvarin@gmail.com>
2016-02-05 21:29:44 -0500

    Fixing tests styling.

--- src/test/GameGridTest.java
+++ src/test/GameGridTest.java
@@ -1,77 +1,75 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 import java.io.IOException;
+
 import org.junit.Test;
+
 import model.GameGrid;
 import model.GridLocation;
+
 public class GameGridTest {
 
-    GameGrid testgamegird=new GameGrid();
-    GameGrid badgamegird=new GameGrid();
-      
-  
+    GameGrid testgamegird = new GameGrid();
+    GameGrid badgamegird = new GameGrid();
+
     @Test
     public void testReadFromFile() throws IOException {
         testgamegird.readFromFile("src/test/testfiles/testmap.txt");
-        assertTrue("The information could not be read from the file",testgamegird.cases.length==10);
+        assertTrue("The information could not be read from the file",
+                        testgamegird.cases.length == 10);
     }
-    
-    
+
     @Test
-    public void testWriteToFile() throws IOException{
+    public void testWriteToFile() throws IOException {
         testgamegird.readFromFile("src/test/testfiles/testmap.txt");
         testgamegird.writeToFile("src/test/testfiles/testmapWrite.txt");
         File testfile = new File("src/test/testfiles/testmapWrite.txt");
-        assertTrue("The file coud not be written",testfile.exists());
+        assertTrue("The file coud not be written", testfile.exists());
         testfile.delete();
-  
+
     }
 
     @Test
     public void testGetCases() {
         testgamegird.readFromFile("src/test/testfiles/testmap.txt");
-        int[][] testcases=testgamegird.getCases();
-        assertTrue("getCases method can't retrieve the array",testcases.length>0);
-       
-    }
+        int[][] testcases = testgamegird.getCases();
+        assertTrue("getCases method can't retrieve the array", testcases.length > 0);
 
-  
+    }
 
     @Test
     public void testIsConnected() {
         testgamegird.readFromFile("src/test/testfiles/testmap.txt");
-        assertTrue("IsConnected failed for a connected map",testgamegird.isConnected());
+        assertTrue("IsConnected failed for a connected map", testgamegird.isConnected());
         badgamegird.readFromFile("src/test/testfiles/testmap2.txt");
-        assertTrue("IsConnected failed for a not connected map",!badgamegird.isConnected());
+        assertTrue("IsConnected failed for a not connected map", !badgamegird.isConnected());
     }
-    
+
     @Test
     public void testEntryPoint() {
         testgamegird.readFromFile("src/test/testfiles/testmap.txt");
-        GridLocation entry=testgamegird.entryPoint();
-        assertTrue("entryPoint() failed to work",entry.xCoordinate>-1 && entry.yCoordinate>-1);
-            
+        GridLocation entry = testgamegird.entryPoint();
+        assertTrue("entryPoint() failed to work", entry.xCoordinate > -1 && entry.yCoordinate > -1);
+
     }
-    
-    
+
     @Test
     public void testExitPoint() {
         testgamegird.readFromFile("src/test/testfiles/testmap.txt");
-        GridLocation exit=testgamegird.exitPoint();
-        assertTrue("exitPoint() failed to work",exit.xCoordinate>-1 && exit.yCoordinate>-1);
-            
-    }   
-    
+        GridLocation exit = testgamegird.exitPoint();
+        assertTrue("exitPoint() failed to work", exit.xCoordinate > -1 && exit.yCoordinate > -1);
+
+    }
 
     @Test
     public void testGridValid() {
         testgamegird.readFromFile("src/test/testfiles/testmap.txt");
-        GridLocation testgridl=testgamegird.exitPoint();
-        assertTrue("GridValid test failed for a vlid GridLocation",testgamegird.gridValid(testgridl));        
+        GridLocation testgridl = testgamegird.exitPoint();
+        assertTrue("GridValid test failed for a vlid GridLocation",
+                        testgamegird.gridValid(testgridl));
     }
-
-  
 
 }
