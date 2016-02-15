d80c465820afbc888c735ca02a59dd20c9547391
Louis-Bertrand Varin <louisbvarin@gmail.com>
2016-02-14 19:37:39 -0500

    Adding the missing exception class.

---
+++ src/model/GameGridException.java
@@ -0,0 +1,20 @@
+package model;
+
+/**
+ * Base class for the exceptions raised in the GameGrid class.
+ *
+ * @author Louis-Bertrand Varin
+ *
+ */
+public class GameGridException extends Exception {
+
+    public GameGridException(String string) {
+        super(string);
+    }
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+}
