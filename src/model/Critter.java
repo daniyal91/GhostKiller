14a1fb213d19e2e9cbe592a21b817190772eeb53
Louis-Bertrand Varin <louisbvarin@gmail.com>
2016-03-09 22:56:57 -0500

    Adding critter characteristics.

--- src/model/Critter.java
+++ src/model/Critter.java
@@ -9,20 +9,32 @@ package model;
 public class Critter {
 
     public GridLocation gridLocation;
-    private int hitPoint;
-    private int strength;
-    private int speed;
+
+    public static int HEALTH_POINTS_PER_LEVEL = 100;
+    public static int SPEED_PER_LEVEL = 20;
+    public static int REWARD_PER_LEVEL = 50;
+
+    private int healthPoints;
     private int level;
-    int health;
 
     public Critter(GridLocation gridLocation, int level) {
         this.gridLocation = gridLocation;
+        this.healthPoints = Critter.HEALTH_POINTS_PER_LEVEL * level;
         this.level = level;
     }
 
     public Critter(Critter critter) {
         this.gridLocation = critter.gridLocation;
-        this.health = critter.health;
+        this.healthPoints = Critter.HEALTH_POINTS_PER_LEVEL * critter.level;
+        this.level = critter.level;
+    }
+
+    public boolean isDead() {
+        return this.healthPoints <= 0;
+    }
+
+    public void takeDamage(int damage) {
+        this.healthPoints -= damage;
     }
 
 }
