b99de2da30b7766698c1bb4e48503bbd11d9facb
khodayar <khodayar.j@gmail.com>
2016-03-13 10:29:34 -0400

    path one new test

--- src/test/PathTestCase.java
+++ src/test/PathTestCase.java
@@ -9,23 +9,22 @@ import model.GridLocation;
 import model.Path;
 
 public class PathTestCase {
-    
-    
+
+
     GameGrid testgamegird= new GameGrid();
     ArrayList <GridLocation> testshortestpath=new ArrayList<GridLocation>();
     Path testpath;
-    
+
     @Before
     public void beforeTest(){
-    testgamegird.readFromFile("src/test/testfiles/testmap.txt",false);
-  //  int [][][] connectivities =testgamegird.connectivities();
-    testpath=new Path(testgamegird);
-    testshortestpath=testpath.getShortestPath();
+        testgamegird.readFromFile("src/test/testfiles/testmap.txt",false);
+        testpath=new Path(testgamegird);
+        testshortestpath=testpath.getShortestPath();
     }
-     
+
     @Test
     public void testShortestPath() {
-       //tests the start point of the shortest path
+        //tests the start point of the shortest path
         assertTrue(testshortestpath.get(0).equals(new GridLocation(1,2)));
         //tests the length of the shortest path
         assertTrue(testshortestpath.size()==10);
@@ -39,7 +38,15 @@ public class PathTestCase {
         GridLocation next=new GridLocation(4,5);
         assertTrue(testpath.getNextLocation(current).equals(next));       
     }
- 
     
-    
+    @Test
+    public void testConnectivityofShortestPath() {
+        GridLocation step=testshortestpath.get(0);
+        for (int i=1;i<testshortestpath.size();i++) {
+            step=testpath.getNextLocation(step);
+        }     
+        assertTrue(step.equals(new GridLocation(4,8)));       
+    }
+
+
 }
