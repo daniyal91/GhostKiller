83b440d55d2e75344456b22332ce62eba08d3ac9
khodayar <khodayar.j@gmail.com>
2016-04-02 09:04:28 -0400

    working on merging problem

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -20,89 +20,102 @@ import views.MainView;
  */
 public class MainController implements Runnable, ActionListener {
 
-	private MainView mainFrame;
+    private MainView mainFrame;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void run() {
-		try {
-			this.mainFrame = new MainView(this);
-			this.mainFrame.setVisible(true);
-		} catch (Exception exception) {
-			exception.printStackTrace();
-		}
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void run() {
+        try {
+            this.mainFrame = new MainView(this);
+            this.mainFrame.setVisible(true);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * Can execute one of the following 3 actions :
-	 * * Launch the map editor with a new map;
-	 * * Launch the map editor with an existing map;
-	 * * Launch the game with a selected map.
-	 */
-	@Override
-	public void actionPerformed(ActionEvent event) {
-		if (event.getSource() == this.mainFrame.buttonCreate) {
+    /**
+     * {@inheritDoc}
+     *
+     * Can execute one of the following 3 actions :
+     * * Launch the map editor with a new map;
+     * * Launch the map editor with an existing map;
+     * * Launch the game with a selected map.
+     */
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        if (event.getSource() == this.mainFrame.buttonCreate) {
 
-			String lineText = this.mainFrame.textFieldLines.getText();
-			int lineCount = Integer.parseInt(lineText);
-			String columnText = this.mainFrame.textFieldColumns.getText();
-			int columnCount = Integer.parseInt(columnText);
+            int lineCount;
+            int columnCount;
 
-			if (lineCount < 5) {
-				JOptionPane.showMessageDialog(null, "The height of the new grid must be at least 5!", "Invalid dimensions", JOptionPane.WARNING_MESSAGE);
-				return;
-			}
+            String lineText = this.mainFrame.textFieldLines.getText();
+            try {
+                lineCount = Integer.parseInt(lineText);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "The number of lines must be an integer!", "Invalid lines value", JOptionPane.WARNING_MESSAGE);
+                return;
+            }
 
-			if (columnCount < 5) {
-				JOptionPane.showMessageDialog(null, "The width of the new grid must be at least 5!", "Invalid dimensions", JOptionPane.WARNING_MESSAGE);
-				return;
-			}
+            String columnText = this.mainFrame.textFieldColumns.getText();
+            try {
+                columnCount = Integer.parseInt(columnText);
+            } catch(NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "The number of columns must be an integer!", "Invalid columns value", JOptionPane.WARNING_MESSAGE);
+                return;
+            }
 
-			GameGrid gameGrid = new GameGrid(lineCount, columnCount);
-			EditMapController controller = new EditMapController(gameGrid);
+            if (lineCount < 5) {
+                JOptionPane.showMessageDialog(null, "The height of the new grid must be at least 5!", "Invalid dimensions", JOptionPane.WARNING_MESSAGE);
+                return;
+            }
 
+            if (columnCount < 5) {
+                JOptionPane.showMessageDialog(null, "The width of the new grid must be at least 5!", "Invalid dimensions", JOptionPane.WARNING_MESSAGE);
+                return;
+            }
 
-		} else if (event.getSource() == this.mainFrame.buttonEdit) {
-
-			String filePath = Utils.selectFile();
-			if (filePath != null) {
-
-				GameGrid gameGrid = new GameGrid();
-				gameGrid.readFromFile(filePath, false);
-				EditMapController controller = new EditMapController(gameGrid);
-
-			}
-
-		} else if (event.getSource() == this.mainFrame.buttonNewGame) {
-			String filePath = Utils.selectFile();
-			if (filePath != null) {
-
-				Game game = new Game();
-				game.grid.readFromFile(filePath, true);
-				GameController gameController = new GameController(game);
-
-				Path t=new Path(game.grid);
-				System.out.println("shortest path , critters' path towrad exit point :");
-				System.out.println(t.getShortestPath());
+            GameGrid gameGrid = new GameGrid(lineCount, columnCount);
+            EditMapController controller = new EditMapController(gameGrid);
 
 
+        } else if (event.getSource() == this.mainFrame.buttonEdit) {
 
-			}
-		} else if (event.getSource() == this.mainFrame.buttonLoad) {
-			String filePath = Utils.selectFile();
-			if (filePath != null) {
+            String filePath = Utils.selectFile();
+            if (filePath != null) {
 
-				Game game=new Game();
-				game.loadGame(filePath);
-				GameController gameController = new GameController(game);
-			}
-		}
+                GameGrid gameGrid = new GameGrid();
+                gameGrid.readFromFile(filePath, false);
+                EditMapController controller = new EditMapController(gameGrid);
 
-	}
+            }
+
+        } else if (event.getSource() == this.mainFrame.buttonNewGame) {
+            String filePath = Utils.selectFile();
+            if (filePath != null) {
+
+                Game game = new Game();
+                game.grid.readFromFile(filePath, true);
+                GameController gameController = new GameController(game);
+
+                Path t=new Path(game.grid);
+                System.out.println("shortest path , critters' path towrad exit point :");
+                System.out.println(t.getShortestPath());
+
+
+
+            }
+        } else if (event.getSource() == this.mainFrame.buttonLoad) {
+            String filePath = Utils.selectFile();
+            if (filePath != null) {
+
+                Game game=new Game();
+                game.loadGame(filePath);
+              }
+        }
+
+    }
 
 
 
